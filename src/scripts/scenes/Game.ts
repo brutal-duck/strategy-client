d2b79c604304376be54a9845f5b2fa8b9a3baf70
Vladimir Rodionov <vsrodionov94@yandex.ru>
2021-12-14 13:17:29 +0700

    add clear all

--- src/scripts/scenes/Game.ts
+++ src/scripts/scenes/Game.ts
@@ -110,6 +110,12 @@ export default class Game extends Phaser.Scene {
     this.enemyColor = this.player.color === 'red' ? 'green' : 'red'
     this.green = Object.assign({}, config)
     this.red = Object.assign({}, config)
+    if (this.state.game.AI !== '') {
+      this.AI = new AI(this, this.state.game.AI)
+      this.AI.init()
+      this.state.enemy.name = '';
+    }
+
     this[this.player.color].name = this.state.player.name;
     this[this.enemyColor].name = this.state.enemy?.name || this.lang.enemy;
     if (this[this.enemyColor].name === this.lang.you) this[this.enemyColor].name = this.lang.enemy;
@@ -127,10 +133,7 @@ export default class Game extends Phaser.Scene {
     this.gameIsOn = true // запущен ли матч
     this.world.recreate(this.gameIsOn, this.state.game.seed);
     
-    if (this.state.game.AI !== '') {
-      this.AI = new AI(this, this.state.game.AI)
-      this.AI.init()
-    }
+
 
     this.input.keyboard.createCursorKeys().space.on('down', (): void => {
       if (this.state.platform === platforms.VK) {
